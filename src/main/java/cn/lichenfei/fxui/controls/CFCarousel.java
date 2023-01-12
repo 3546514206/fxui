5fcafab694a0fbe8ea4c3463feb61109921a5785
lichenfei <lichenfei_fei@163.com>
2023-01-12 14:02:02 +0800

    轮播测试

--- src/main/java/cn/lichenfei/fxui/controls/CFCarousel.java
+++ src/main/java/cn/lichenfei/fxui/controls/CFCarousel.java
@@ -2,6 +2,7 @@ package cn.lichenfei.fxui.controls;
 
 import cn.lichenfei.fxui.common.FxUtils;
 import javafx.animation.*;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
@@ -18,8 +19,8 @@ import java.util.List;
  */
 public class CFCarousel extends StackPane {
 
-    private Duration interval = Duration.millis(1000);// 自动切换的时间间隔
-    private Duration duration = Duration.millis(1000); // 动画移动时间
+    private Duration interval = Duration.millis(3000);// 隔多少时间切换下一个
+    private Duration duration = Duration.millis(1000); // 动画时间
     private List<StackPane> spList = new ArrayList<>();// 需要轮播的容器列表
     private int currentIndex = 1;
 
@@ -42,7 +43,7 @@ public class CFCarousel extends StackPane {
         StackPane sp3 = new StackPane();
         sp3.getChildren().add(new ImageView(FxUtils.getImage("/img/img3.png")));
         this.spList = Arrays.asList(sp1, sp2, sp3);*/
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 6; i++) {
             this.spList.add(getSp((i + 1) + ""));
         }
         //
@@ -51,38 +52,42 @@ public class CFCarousel extends StackPane {
         this.setPrefHeight(300);
         this.setMaxWidth(Double.NEGATIVE_INFINITY);
         this.setMaxHeight(Double.NEGATIVE_INFINITY);
-        setEvent();
+        setAnimation();
     }
 
-    private void setEvent() {
+    /**
+     * 设置动画效果
+     */
+    private void setAnimation() {
         if (spList.size() == 1) {
             this.getChildren().add(spList.get(0));
             return;
         }
         spList.get(1).setTranslateX(-500);
-        this.getChildren().addAll(spList.get(1), spList.get(0));
-        // 动画效果
+        this.getChildren().addAll(spList.get(0), spList.get(1));
+        // 动画效果（根据动画设置节点的偏移量）
         button.translateXProperty().addListener((observable, oldValue, newValue) -> {
-            this.getChildren().get(0).setTranslateX(currentIndex % 2 == 0 ? newValue.doubleValue() - 500 : newValue.doubleValue());
-            this.getChildren().get(1).setTranslateX(currentIndex % 2 == 1 ? newValue.doubleValue() - 500 : newValue.doubleValue());
+            Node node1 = this.getChildren().get(0);
+            Node node2 = this.getChildren().get(1);
+            node1.setTranslateX(currentIndex % 2 == 0 ? newValue.doubleValue() - 500 : newValue.doubleValue());
+            node2.setTranslateX(currentIndex % 2 == 1 ? newValue.doubleValue() - 500 : newValue.doubleValue());
         });
-
-        TT.setOnFinished(event -> {// 每次轮播执行完毕
+        // 每次轮播执行完毕
+        TT.setOnFinished(event -> {
+            System.out.println("currentIndex：" + currentIndex);
+            StackPane leftNode;
+            int setIndex;
             if (currentIndex + 1 == spList.size()) {
+                leftNode = spList.get(0);
+                setIndex = currentIndex % 2 == 0 ? 1 : 0;
                 currentIndex = 0;
             } else {
                 currentIndex++;
+                leftNode = spList.get(currentIndex);
+                setIndex = currentIndex % 2 == 1 ? 1 : 0;
             }
-            System.out.println("currentIndex：" + currentIndex);
-            if (currentIndex % 2 == 1) {
-                StackPane left = spList.get(currentIndex);
-                left.setTranslateX(-500);
-                this.getChildren().set(1, left);
-            } else {
-                StackPane right = spList.get(currentIndex);
-                right.setTranslateX(-500);
-                this.getChildren().set(0, right);
-            }
+            leftNode.setTranslateX(-500);
+            this.getChildren().set(setIndex, leftNode);
         });
         TT.setFromX(0);
         TT.setToX(500);
@@ -101,5 +106,4 @@ public class CFCarousel extends StackPane {
         stackPane.setStyle("-fx-background-color:rgb(255,255,255);");
         return stackPane;
     }
-
 }
