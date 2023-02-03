04333144bdfc4d54616f9f7816f26b7282657779
lichenfei <lichenfei_fei@163.com>
2023-02-03 14:37:34 +0800

    加载效果案例

---
+++ src/main/java/cn/lichenfei/fxui/controls/CFLoading.java
@@ -0,0 +1,147 @@
+package cn.lichenfei.fxui.controls;
+
+import javafx.animation.Interpolator;
+import javafx.animation.ParallelTransition;
+import javafx.animation.RotateTransition;
+import javafx.animation.TranslateTransition;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.text.Font;
+import javafx.util.Duration;
+
+public class CFLoading extends StackPane {
+
+    // 大小
+    private Size defaultSize;
+
+    // 动画相关
+    private Button TRANSITION_NODE = new Button();
+    private TranslateTransition TT = new TranslateTransition();
+    private RotateTransition RT = new RotateTransition();
+    private ParallelTransition PT = new ParallelTransition(TT, RT);
+
+    // 加载圈
+    private Arc arc1;
+    private Arc arc2;
+
+    // 布局
+    private Pane arcPane = new Pane();
+    private Label message = new Label("Loading...");
+
+    public CFLoading(CFLoading.Size size) {
+        this.defaultSize = size;
+        setLayout();
+        setAnimation();
+    }
+
+    public CFLoading setMessage(String message) {
+        this.message.setText(message);
+        return this;
+    }
+
+    public CFLoading setColor(Color color) {
+        arc2.setStroke(color);
+        return this;
+    }
+
+    public CFLoading setBackColor(Color color) {
+        arc1.setStroke(color);
+        return this;
+    }
+
+    // 布局
+    private void setLayout() {
+        this.setMaxHeight(Double.NEGATIVE_INFINITY);
+        this.setMaxWidth(Double.NEGATIVE_INFINITY);
+        switch (defaultSize) {
+            case SMALL:
+                this.getChildren().addAll(this.arcPane);
+                break;
+            case NORMAL:
+                VBox vBox = new VBox();
+                vBox.setSpacing(10);
+                vBox.setAlignment(Pos.CENTER);
+                vBox.getChildren().addAll(this.arcPane, this.message);
+                this.arcPane.setMaxWidth(Double.NEGATIVE_INFINITY);
+                this.getChildren().addAll(vBox);
+                break;
+            case LARGE:
+                this.getChildren().addAll(this.arcPane, this.message);
+                break;
+            default:
+                break;
+        }
+        this.arc1 = getArc(360, Color.valueOf("#dedfe0"));
+        this.arc2 = getArc(50, Color.valueOf("#409EFF"));
+        this.arcPane.getChildren().addAll(arc1, arc2);
+        this.arcPane.setPrefWidth(defaultSize.getSize());
+        this.arcPane.setPrefHeight(defaultSize.getSize());
+        this.message.setFont(new Font(14));
+        this.message.setTextFill(Color.valueOf("#409EFF"));
+    }
+
+    private Arc getArc(double arcLength, Color color) {
+        double size = this.defaultSize.getSize() / 2;
+        Arc arc = new Arc(size, size, size, size, 0, arcLength);
+        arc.setFill(null);
+        arc.setStrokeWidth(this.defaultSize.getStrokeWidth());
+        arc.setStroke(color);
+        return arc;
+    }
+
+    /**
+     * 动画
+     */
+    private void setAnimation() {
+        //动画监听
+        arc2.lengthProperty().bind(TRANSITION_NODE.translateXProperty());
+        arc2.startAngleProperty().bind(TRANSITION_NODE.rotateProperty());
+
+        //移动动画
+        TT.setNode(TRANSITION_NODE);
+        TT.setFromX(270);
+        TT.setToX(0);
+        TT.setDuration(Duration.millis(1000));
+        TT.setInterpolator(Interpolator.LINEAR);
+        TT.setAutoReverse(true);
+        TT.setCycleCount(-1);
+        //旋转动画
+        RT.setNode(TRANSITION_NODE);
+        RT.setFromAngle(360);
+        RT.setToAngle(0);
+        RT.setDuration(Duration.millis(700));
+        RT.setInterpolator(Interpolator.LINEAR);
+        RT.setCycleCount(-1);
+        //
+        PT.play();
+    }
+
+    // 加载容器大小
+    public enum Size {
+        LARGE(80, 4),
+        NORMAL(50, 3),
+        SMALL(15, 2);
+
+        Size(double size, double strokeWidth) {
+            this.size = size;
+            this.strokeWidth = strokeWidth;
+        }
+
+        private double size;
+        private double strokeWidth;
+
+        public double getSize() {
+            return size;
+        }
+
+        public double getStrokeWidth() {
+            return strokeWidth;
+        }
+    }
+}
--- src/main/java/cn/lichenfei/fxui/demo/LoadingDemo.java
+++ src/main/java/cn/lichenfei/fxui/demo/LoadingDemo.java
@@ -1,90 +1,78 @@
 package cn.lichenfei.fxui.demo;
 
-import javafx.animation.Interpolator;
-import javafx.animation.ParallelTransition;
-import javafx.animation.RotateTransition;
-import javafx.animation.TranslateTransition;
+import cn.lichenfei.fxui.common.FxUtils;
+import cn.lichenfei.fxui.common.Level;
+import cn.lichenfei.fxui.common.LoadingUtils;
+import cn.lichenfei.fxui.controls.CFLoading;
+import cn.lichenfei.fxui.controls.SimpleButton;
 import javafx.application.Application;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Arc;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
+import java.util.concurrent.TimeUnit;
 
 public class LoadingDemo extends Application {
 
-    double width = 100;
-
-    Button button = new Button();
-    TranslateTransition t = new TranslateTransition();
-    RotateTransition r = new RotateTransition();
-    ParallelTransition pt = new ParallelTransition(t, r);
-    //
-    private Arc arc1 = getArc(width, 360, Color.valueOf("#dedfe0"));
-    private Arc arc2 = getArc(width, 50, Color.valueOf("#409EFF"));
-
-    private Arc getArc(double size, double arcLength, Color color) {
-        Arc arc = new Arc(size / 2, size / 2, size / 2, size / 2, 0, arcLength);
-        arc.setFill(null);
-        arc.setStrokeWidth(4);
-        arc.setStroke(color);
-        return arc;
-    }
-
-    public Parent createContent() {
-        Pane root = new Pane();
-        root.setMaxHeight(Double.NEGATIVE_INFINITY);
-        root.setMaxWidth(Double.NEGATIVE_INFINITY);
-        root.setPrefWidth(width);
-        root.setPrefHeight(width);
-        root.getChildren().addAll(arc1, arc2);
-
-        //移动动画
-        t.setNode(button);
-        t.setFromX(200);
-        t.setToX(30);
-        t.setDuration(Duration.millis(1000));
-        t.setAutoReverse(true);
-        t.setCycleCount(-1);
-        //
-        r.setNode(button);
-        r.setFromAngle(0);
-        r.setToAngle(360);
-        r.setDuration(Duration.millis(700));
-        r.setInterpolator(Interpolator.LINEAR);
-        r.setCycleCount(-1);
-        //
-        pt.play();
-
-        arc2.lengthProperty().bind(button.translateXProperty());
-        arc2.startAngleProperty().bind(button.rotateProperty());
-
-        StackPane stackPane = new StackPane(root);
-        Label message = new Label("Loading...");// 提示文字
-        message.setFont(new Font(14));
-        message.setTextFill(Color.valueOf("#409EFF"));
-        stackPane.getChildren().add(message);
-        return stackPane;
-    }
-
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setScene(new Scene(createContent()));
-        primaryStage.setWidth(500);
+        HBox hBox = new HBox();
+        StackPane root = new StackPane(hBox);
+        hBox.setMaxHeight(Double.NEGATIVE_INFINITY);
+        hBox.setMaxWidth(Double.NEGATIVE_INFINITY);
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setSpacing(50);
+        //按钮加载
+        Button button1 = SimpleButton.get("按钮加载", Level.PRIMARY);
+        button1.setOnMouseClicked(event ->
+                LoadingUtils.buttonLoad(button1, () -> {
+                    // 在这个里边可以执行业务逻辑
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }));
+
+        //指定容器
+        Button button2 = SimpleButton.get("指定容器", Level.SUCCESS);
+        button2.setOnMouseClicked(event ->
+                LoadingUtils.nodeLoad(root, () -> {
+                    // 在这个里边可以执行业务逻辑
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }));
+        //指定大小
+        Button button3 = SimpleButton.get("指定大小", Level.INFO);
+        button3.setOnMouseClicked(event ->
+                LoadingUtils.nodeLoad(root, new CFLoading(CFLoading.Size.LARGE).setMessage("稍等片刻!"), () -> {
+                    // 在这个里边可以执行业务逻辑
+                    try {
+                        TimeUnit.SECONDS.sleep(3);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    return true;
+                }));
+
+        hBox.getChildren().addAll(button1, button2, button3);
+        //
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(FxUtils.getCss("/css/core.css"));// 加载css
+        primaryStage.setScene(scene);
+        primaryStage.setWidth(700);
         primaryStage.setHeight(400);
         primaryStage.show();
     }
 
-    /**
-     * Java main for when running without JavaFX launcher
-     */
     public static void main(String[] args) {
         launch(args);
     }
