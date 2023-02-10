f801dd649266b7007f8ed227306143329edca6a6
lichenfei <lichenfei_fei@163.com>
2023-02-10 16:03:13 +0800

    登录界面雏形

--- src/main/java/cn/lichenfei/fxui/ui/Login.java
+++ src/main/java/cn/lichenfei/fxui/ui/Login.java
@@ -1,132 +1,123 @@
 package cn.lichenfei.fxui.ui;
 
-import cn.lichenfei.fxui.common.FxUtils;
 import cn.lichenfei.fxui.controls.CFButton;
 import cn.lichenfei.fxui.controls.CFTextField;
-import javafx.beans.binding.DoubleBinding;
-import javafx.beans.property.SimpleDoubleProperty;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Control;
-import javafx.scene.effect.Lighting;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.LinearGradient;
-import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import org.kordamp.ikonli.antdesignicons.AntDesignIconsOutlined;
 import org.kordamp.ikonli.javafx.FontIcon;
 
+import java.util.function.Consumer;
+
 public class Login extends StackPane {
 
-    // 基本属性
-    private DoubleBinding DEFAULT_WIDTH = widthProperty().subtract(100);
-    private DoubleBinding DEFAULT_HEIGHT = new SimpleDoubleProperty(45).subtract(0);
     // 登录，注册容器
     private SignInBox signInBox = new SignInBox();
     private SignUpBox signUpBox = new SignUpBox();
 
+    //动画
+    private TranslateTransition TT = new TranslateTransition(Duration.millis(500));
+    private FadeTransition RT = new FadeTransition(Duration.millis(500));
+    private ParallelTransition PT = new ParallelTransition(TT, RT);
+
     public Login() {
-        getChildren().addAll(signInBox, signUpBox);
-        signInBox.translateYProperty().bind(heightProperty());
-        //signUpBox.translateYProperty().bind(heightProperty());
-
+        getChildren().addAll(signInBox);
+        //动画监听
+        signInBox.toSignUpClicked(event -> {
+            play(this.signInBox, false);
+        });
+        signUpBox.toSignInClicked(event -> {
+            play(this.signUpBox, false);
+        });
+        setSignInBoxSignUpStyle(this.signUpBox);
+        setSignInBoxSignUpStyle(this.signInBox);
     }
 
-    /**
-     * 绑定宽度和高度
-     *
-     * @param node
-     */
-    private void bindWidthHeight(Region... node) {
-        for (int i = 0; i < node.length; i++) {
-            node[i].prefWidthProperty().bind(DEFAULT_WIDTH);
-            node[i].prefHeightProperty().bind(DEFAULT_HEIGHT);
-        }
+    private void play(Node node, boolean in) {
+        RT.setNode(node);
+        RT.setFromValue(in ? 0 : 1);
+        RT.setToValue(in ? 1 : 0);
+        //
+        TT.setNode(node);
+        TT.setFromY(in ? 300 : 0);
+        TT.setToY(in ? 0 : 300);
+        //
+        PT.setAutoReverse(true);
+        PT.setCycleCount(2);
+        PT.play();
     }
 
-    /**
-     * 背景
-     */
-    public class Backdrop extends StackPane {
-        private Image image;
-        private ImageView imageView = new ImageView();
-        private Rectangle rectangle = new Rectangle();
-
-        public Backdrop(Image image) {
-            this.image = image;
-            setLayout();
-        }
-
-        private void setLayout() {
-            getChildren().addAll(imageView, rectangle);
-            StackPane.setAlignment(imageView, Pos.BOTTOM_CENTER);
-            StackPane.setAlignment(rectangle, Pos.BOTTOM_CENTER);
-            imageView.setImage(image);
-            imageView.fitWidthProperty().bind(widthProperty());
-            imageView.setEffect(new Lighting());
-            imageView.fitHeightProperty().bind(heightProperty().divide(2));
-            rectangle.heightProperty().bind(imageView.fitHeightProperty());
-            rectangle.widthProperty().bind(imageView.fitWidthProperty());
-            //背景渐变
-            Stop[] stops = new Stop[]{new Stop(0, Color.WHITE), new Stop(1, Color.rgb(255, 255, 255, 0))};
-            LinearGradient lg1 = new LinearGradient(1, 0, 1, 1, true, CycleMethod.NO_CYCLE, stops);
-            rectangle.setFill(lg1);
-        }
+    // 设置登录，注册容器样式
+    private void setSignInBoxSignUpStyle(VBox node) {
+        node.setStyle("-fx-background-color:rgb(242,242,242);-fx-background-radius:5px;");
+        node.setAlignment(Pos.BOTTOM_CENTER);
+        node.setSpacing(20);
+        node.setPadding(new Insets(50, 0, 50, 0));
+        node.prefWidthProperty().bind(widthProperty().subtract(50));
+        node.setMaxHeight(Control.USE_PREF_SIZE);
+        node.setMaxWidth(Control.USE_PREF_SIZE);
+        StackPane.setAlignment(node, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(node, new Insets(0, 0, 50, 0));
     }
-
 
     /**
      * 登录
      */
-    public class SignInBox extends StackPane {
+    public class SignInBox extends VBox {
 
-        private VBox container = new VBox();
         private CFTextField email = new CFTextField(CFTextField.Type.TEXT, new FontIcon(AntDesignIconsOutlined.MAIL));
         private CFTextField password = new CFTextField(CFTextField.Type.PASSWORD, new FontIcon(AntDesignIconsOutlined.KEY));
         private CFButton signIn = new CFButton("登录");
+        private Hyperlink toSignUp = new Hyperlink("没有账户？去注册！");
 
         public SignInBox() {
-            getChildren().addAll(new Backdrop(FxUtils.getImage("/img/backdrop1.png")), container);
-            container.getChildren().addAll(email, password, signIn);
+            getChildren().addAll(email, password, signIn, toSignUp);
             signIn.prefWidthProperty().bind(password.widthProperty());
-            container.setAlignment(Pos.TOP_CENTER);
-            container.setSpacing(20);
-            container.setPadding(new Insets(100, 0, 0, 0));
-            bindWidthHeight(email, password, signIn);
             //
             email.setPromptText("邮箱");
             password.setPromptText("密码");
         }
+
+
+        public void toSignUpClicked(Consumer<MouseEvent> consumer) {
+            this.toSignUp.setOnMouseClicked(event -> consumer.accept(event));
+        }
+
     }
 
     /**
      * 注册
      */
-    public class SignUpBox extends StackPane {
+    public class SignUpBox extends VBox {
 
-        private VBox container = new VBox();
         private CFTextField user = new CFTextField(CFTextField.Type.TEXT, new FontIcon(AntDesignIconsOutlined.USER));
         private CFTextField email = new CFTextField(CFTextField.Type.TEXT, new FontIcon(AntDesignIconsOutlined.MAIL));
         private CFTextField password = new CFTextField(CFTextField.Type.PASSWORD, new FontIcon(AntDesignIconsOutlined.KEY));
         private CFButton signUp = new CFButton("注册");
+        private Hyperlink toSignIn = new Hyperlink("已有账户？去登录！");
 
         public SignUpBox() {
-            getChildren().addAll(new Backdrop(FxUtils.getImage("/img/backdrop2.png")), container);
-            container.getChildren().addAll(user, email, password, signUp);
+            getChildren().addAll(user, email, password, signUp, toSignIn);
             signUp.prefWidthProperty().bind(password.widthProperty());
-            container.setAlignment(Pos.TOP_CENTER);
-            container.setSpacing(20);
-            container.setPadding(new Insets(100, 0, 0, 0));
-            bindWidthHeight(user, email, password, signUp);// 绑定组件宽度和高度
             //
             user.setPromptText("用户名");
             email.setPromptText("邮箱");
             password.setPromptText("密码");
+        }
+
+        public void toSignInClicked(Consumer<MouseEvent> consumer) {
+            this.toSignIn.setOnMouseClicked(event -> consumer.accept(event));
         }
     }
 
@@ -136,6 +127,25 @@ public class Login extends StackPane {
             super(nodes);
             setMaxHeight(Control.USE_PREF_SIZE);
             setMaxWidth(Control.USE_PREF_SIZE);
+        }
+    }
+
+    /**
+     * 背景
+     */
+    public class Backdrop extends StackPane {
+        private Image image;
+        private ImageView imageView = new ImageView();
+
+        public Backdrop(Image image) {
+            this.image = image;
+            setLayout();
+        }
+
+        private void setLayout() {
+            getChildren().addAll(imageView);
+            StackPane.setAlignment(imageView, Pos.TOP_CENTER);
+            imageView.setImage(image);
         }
     }
 }
