a12ddb693c7e01b77d40a5737a455a00362e14f7
lichenfei <lichenfei_fei@163.com>
2023-02-21 10:01:57 +0800

    修改登录背景图片

--- src/main/java/cn/lichenfei/fxui/FxApplication.java
+++ src/main/java/cn/lichenfei/fxui/FxApplication.java
@@ -4,6 +4,7 @@ import cn.lichenfei.fxui.common.FxUtil;
 import cn.lichenfei.fxui.controls.CFHeader;
 import cn.lichenfei.fxui.controls.CFStage;
 import cn.lichenfei.fxui.project.Index;
+import com.sun.javafx.application.HostServicesDelegate;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -14,13 +15,16 @@ import javafx.stage.Stage;
 public class FxApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
+
         //基础尺寸
-        CFStage stage = new CFStage(new Index(), 800, 500)
+        final CFStage stage = new CFStage(new Index(), 800, 500)
                 .setHeaderStyle(CFHeader.HeaderStyle.CLOSE)
                 .setBackdropImage(FxUtil.getImage("/img/backdrop.png"));
         stage.setTitle("chenfei-fxui");// 标题
         stage.getIcons().add(FxUtil.getImage("/img/logo.jpg"));// icon
         stage.show();
+
+        //HostServicesDelegate instance = HostServicesDelegate.getInstance(this);
     }
 
     public static void main(String[] args) {
