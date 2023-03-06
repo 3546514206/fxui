94c3eb4cd71e01f1b3567d9b5d509429af8ced8e
lichenfei <lichenfei_fei@163.com>
2023-03-06 18:06:33 +0800

    用户详情界面

--- src/main/java/cn/lichenfei/fxui/project/UserDetail.java
+++ src/main/java/cn/lichenfei/fxui/project/UserDetail.java
@@ -2,22 +2,29 @@ package cn.lichenfei.fxui.project;
 
 import cn.lichenfei.fxui.FxApplication;
 import cn.lichenfei.fxui.common.FxUtil;
+import cn.lichenfei.fxui.common.Level;
 import cn.lichenfei.fxui.common.SimpleControl;
 import cn.lichenfei.fxui.controls.CFAvatar;
 import cn.lichenfei.fxui.controls.CFPopup;
 import javafx.application.HostServices;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class UserDetail extends VBox {
 
     private static final String STYLE_SHEET = FxUtil.getResource("/css/project/user-detail.css");
     //
     private CFAvatar cfAvatar = new CFAvatar(FxUtil.getImage("/img/avatar.png"), 100, 100);
-    private Label nameLabel = new Label("lichenfei_fei");
-    private Label emailLabel = new Label("lichenfei_fei@163.com");
+    private Label nameLabel = SimpleControl.getLabel("lichenfei_fei", SimpleControl.LabelEnum.H4);
+    private Hyperlink emailLink = SimpleControl.getHyperlink("lichenfei_fei@163.com", Level.PRIMARY);
     private Label introductionLabel = new Label("1、3、5写bug，2、4、6改bug。");
     //
     private Button gitee = new Button();
@@ -28,7 +35,7 @@ public class UserDetail extends VBox {
     private CFPopup cfPopup;
 
     public UserDetail() {
-        getChildren().addAll(cfAvatar, nameLabel, emailLabel, introductionLabel, buttonBox);
+        getChildren().addAll(cfAvatar, nameLabel, emailLink, introductionLabel, buttonBox);
         gitee.setGraphic(FxUtil.getIconImage("/img/gitee.png", 30));
         javafx.setGraphic(FxUtil.getIconImage("/img/javafx.png", 30));
         reward.setGraphic(FxUtil.getIconImage("/img/reward.png", 30));
@@ -37,12 +44,18 @@ public class UserDetail extends VBox {
         reward.setTooltip(SimpleControl.getTooltip("鼓励一下作者"));
         //
         getStyleClass().add("user-detail");
-        nameLabel.getStyleClass().add("name-label");
-        emailLabel.getStyleClass().add("email-label");
         introductionLabel.getStyleClass().add("introduction-label");
         buttonBox.getStyleClass().add("button-box");
         //
         HostServices hostServices = FxApplication.hostServices;
+        emailLink.setOnMouseClicked(event -> {
+            try {
+                //Desktop.getDesktop().mail();// 打开邮箱发送邮件
+                Desktop.getDesktop().mail(new URI("mailto:" + emailLink.getText()));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        });
         gitee.setOnMouseClicked(event -> hostServices.showDocument("https://gitee.com/lichenfei_fei/chenfei-fxui"));
         javafx.setOnMouseClicked(event -> hostServices.showDocument("https://openjfx.cn/index.html"));
         reward.setOnMouseClicked(event -> {
