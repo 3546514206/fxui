3f773a54e569022dc0949a7c77ecf14d0c6ce61c
lichenfei <lichenfei_fei@163.com>
2023-03-07 14:36:06 +0800

    动态边框效果

--- src/main/java/cn/lichenfei/fxui/project/UserDetail.java
+++ src/main/java/cn/lichenfei/fxui/project/UserDetail.java
@@ -5,7 +5,6 @@ import cn.lichenfei.fxui.common.FxUtil;
 import cn.lichenfei.fxui.common.Level;
 import cn.lichenfei.fxui.common.SimpleControl;
 import cn.lichenfei.fxui.controls.CFAvatar;
-import cn.lichenfei.fxui.controls.CFPopup;
 import javafx.application.HostServices;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
@@ -31,8 +30,6 @@ public class UserDetail extends VBox {
     private Button javafx = new Button();
     private Button reward = new Button();
     private HBox buttonBox = new HBox(gitee, javafx, reward);
-    //
-    private CFPopup cfPopup;
 
     public UserDetail() {
         initLayout();
@@ -71,10 +68,6 @@ public class UserDetail extends VBox {
         HostServices hostServices = FxApplication.hostServices;
         gitee.setOnMouseClicked(event -> hostServices.showDocument("https://gitee.com/lichenfei_fei/chenfei-fxui"));
         javafx.setOnMouseClicked(event -> hostServices.showDocument("https://openjfx.cn/index.html"));
-        reward.setOnMouseClicked(event -> {
-            cfPopup = new CFPopup(new RewardBox());
-            cfPopup.show(this);
-        });
     }
 
     @Override
