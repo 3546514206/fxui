45299639ca254eb7c422f7dc863e92396233e26c
lichenfei <lichenfei_fei@163.com>
2023-03-09 17:20:44 +0800

    readme.md

--- src/main/java/cn/lichenfei/fxui/project/RewardBox.java
+++ src/main/java/cn/lichenfei/fxui/project/RewardBox.java
@@ -11,11 +11,11 @@ import javafx.scene.layout.VBox;
 
 public class RewardBox extends VBox {
 
-    private ImageView wxImage = FxUtil.getIconImage("/img/reward-wx.jpg", 200);
+    private ImageView wxImage = FxUtil.getIconImage("/img/reward-wx.jpg", 180);
     private Label wxLabel = SimpleControl.getLabel("微信", SimpleControl.LabelEnum.H5);
     private VBox wxBox = new VBox(wxImage, wxLabel);
     //
-    private ImageView zfbImage = FxUtil.getIconImage("/img/reward-zfb.jpg", 200);
+    private ImageView zfbImage = FxUtil.getIconImage("/img/reward-zfb.jpg", 180);
     private Label zfbLabel = SimpleControl.getLabel("支付宝", SimpleControl.LabelEnum.H5);
     private VBox zfbBox = new VBox(zfbImage, zfbLabel);
     //
@@ -23,18 +23,20 @@ public class RewardBox extends VBox {
     //
     private Label title = SimpleControl.getLabel("打赏作者", SimpleControl.LabelEnum.H4);
 
+    private Label message = SimpleControl.getLabel("支持一下作者，请作者吃碗油泼面吧！");
+
     public RewardBox() {
         setPadding(new Insets(10));
-        getChildren().addAll(title, hBox);
+        getChildren().addAll(title, hBox, message);
         setAlignment(Pos.CENTER);
         hBox.setPadding(new Insets(10));
-        hBox.setSpacing(10);
+        hBox.setSpacing(30);
         wxBox.setSpacing(10);
         wxBox.setAlignment(Pos.CENTER);
         zfbBox.setSpacing(10);
         zfbBox.setAlignment(Pos.CENTER);
         //
-        wxBox.setStyle("-fx-border-color:-cf-border-color;-fx-border-radius: 3px;");
-        zfbBox.setStyle("-fx-border-color:-cf-border-color;-fx-border-radius: 3px;");
+        wxBox.setStyle("-fx-border-color:-cf-border-color;-fx-border-radius: 3px;-fx-padding:10px;");
+        zfbBox.setStyle("-fx-border-color:-cf-border-color;-fx-border-radius: 3px;fx-padding:10px;");
     }
 }
